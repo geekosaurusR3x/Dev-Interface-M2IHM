b459a38fb9e965c1b57a5a428db417645e2c9976
Vincent Lamouroux <skadprog@gmail.com>
2014-03-09 16:01:27 +0100

    doxygene labeldessinable

--- Shape_Collage/labeldessinable.cpp
+++ Shape_Collage/labeldessinable.cpp
@@ -9,96 +9,12 @@ LabelDessinable::LabelDessinable(QWidget *parent) :
 
 void LabelDessinable::mousePressEvent(QMouseEvent *event) {
     if (event->button() == Qt::LeftButton) {
-             // QPointF pt = mapToScene(event->pos());
-            // mPoints.push_back(pt);
             QPoint pt(event->pos());
             mClick = pt;
             mDraw = true;
-            /*
-            bool intersects = false;
-            if (mPoints.size() > 2) {
-                QPoint previousPt = mPoints.at(1);
-                QPoint newPoint = event->pos();
-
-                QVector<QPoint> temp = mPoints.mid(2, -1);
-                QLineF previousSegment(mPoints.first(), mPoints.at(1));
-                QLineF firstSegment = previousSegment;
-                this->drawLine(firstSegment);
-                QLineF line;
-                float angle = 0.0;
-
-                foreach (QPoint point, temp) {
-                    line = QLineF(previousPt, point);
-                    this->drawLine(line);
-                    angle += previousSegment.angleTo(line);
-                    qDebug() << "PREVIOUS TO LINE - ANGLE: "  << previousSegment.angleTo(line);
-
-                    previousSegment = line;
-                    previousPt = point;
-                }
-
-                line = QLineF(previousPt, pt);
-                angle += previousSegment.angleTo(line);
-                qDebug() << "PREVIOUS TO LINE - ANGLE: "  << previousSegment.angleTo(line);
-
-                this->drawLine(line);
-                previousSegment = line;
-
-                line = QLineF(pt, mPoints.first());
-                angle += previousSegment.angleTo(line);
-                qDebug() << "PREVIOUS TO LINE - ANGLE: "  << previousSegment.angleTo(line);
-
-                this->drawLine(line);
-                previousSegment = line;
-
-                angle += previousSegment.angleTo(firstSegment);
-                qDebug() << "PREVIOUS TO LINE - ANGLE: "  << previousSegment.angleTo(line);
-
-                qDebug() << "ANGLE: " << angle;
-
-
-                float anglesSum = (mPoints.size() + 1)  * 180 -360;
-                qDebug() << "ANGLE SOMME: " << anglesSum;
-
-
-    //            if (angle < 360)
-
-
-
-                QVector<QPoint> temp = mPoints.mid(0, mPoints.size() - 2);
-
-                QLineF newLine(mPoints.last(), pt);
-                qDebug() << "New line: " << newLine.dx() << " - " << newLine.dy();
-                QPoint lastPoint = mPoints.last();
-                // QPoint lastPoint = mPoints.first();
-
-                QPointF* intersection;
-
-                foreach (QPoint polPt,temp) {
-                    qDebug() << "Intersection? laspt: " << lastPoint.rx() << " " << lastPoint.ry() << "";
-                    qDebug() << "temp: " << polPt.rx() << " " << polPt.ry() << "";
-                    QLineF cLine(lastPoint, polPt);
-                    if (cLine.intersect(newLine, NULL) == QLineF::BoundedIntersection) {
-                        qDebug() << "-> INTERSECTION <-";
-                        intersects = true;
-                        break;
-                    } else {
-                        qDebug() << "NO INTERSECTION";
-                    }
-                    lastPoint = polPt;
-                }
-                *
-            }
-
-            if (!intersects) {
-                mPoints.push_back(pt);
-            }
-
-            */
 
         qDebug() << "New point: " << pt.x() << " " << pt.y();
-        // draw(pt);
-}
+    }
 }
 
 
@@ -128,14 +44,6 @@ void LabelDessinable::mouseReleaseEvent(QMouseEvent *event)
         mLastClickPos = event->pos();
         mDraw = false;
     }
-}
-
-void LabelDessinable::drawLine(QLineF line)  {
-    // TODO Draw
-    QPixmap pixmap = pixmap;
-    QPainter painter(&pixmap);
-    painter.drawLine(line);
-    this->setPixmap(pixmap);
 }
 
 void LabelDessinable::setPreviewLabel(LabelDessinable *value)
--- Shape_Collage/labeldessinable.h
+++ Shape_Collage/labeldessinable.h
@@ -18,28 +18,69 @@ class LabelDessinable : public LabelClicable
     Q_OBJECT
 public:
     explicit LabelDessinable(QWidget *parent = 0);
-    QPolygon getPolygon();
     int getNbVertex() const;
     void setNbVertex(int value);
-    void update();
-    void clear();
     void setPreviewLabel(LabelDessinable *value);
+    QPolygon getPolygon();
+    /**
+     * @brief Demande la remise a jour du polygone en memoire et son redessin
+     */
+    void update();
+    /**
+     * @brief Efface le contenu du label, le polygone et le label de preview si celui ci est present
+     */
+    void clear();
 
 private:
+    /**
+     * @brief Vector de sommets du polygone
+     */
     QVector<QPoint> mPoints;
+    /**
+     * @brief Polygone à afficher
+     */
     QPolygon mPolygon;
+    /**
+     * @brief Qpoint representant la position de la sourie au moment du click gauche
+     */
     QPoint mClick;
+    /**
+     * @brief Qpoint representant la position de la sourie au moment du relachement du boutton gauche
+     */
     QPoint mLastClickPos;
-    bool mDraw;
-    int mNbVertex;
-    void draw();
-    void drawLine(QLineF);
+    /**
+     * @brief Label de previsualisation
+     */
     LabelDessinable* mPreviewLabel;
+    /**
+     * @brief Booleen representant l'etat de dessin
+     */
+    bool mDraw;
+    /**
+     * @brief Nombre de sommet du polygone
+     */
+    int mNbVertex;
+    /**
+     * @brief Dessine le polygone dans le label
+     */
+    void draw();
 signals:
     void clicked();
 public slots:
+    /**
+     * \brief traitement du signal de click souris sur le label
+     * \a event évènement souris
+     */
     void mousePressEvent (QMouseEvent * event);
+    /**
+     * @brief traitement du signal de mouvement de la sourie sur le label
+     * @param event évènement souris
+     */
     void mouseMoveEvent(QMouseEvent* event);
+    /**
+     * @brief traitement du signal de relachement de bouton de la sourie sur le label
+     * @param event évènement souris
+     */
     void mouseReleaseEvent(QMouseEvent* event);
 };
 
