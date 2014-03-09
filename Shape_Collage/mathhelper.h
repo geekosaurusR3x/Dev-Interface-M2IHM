9b51f39be6bfa0451f333f200e9b469e89fd5518
Vincent Lamouroux <skadprog@gmail.com>
2014-03-09 16:06:53 +0100

    doxygene mathhelper

--- Shape_Collage/mathhelper.h
+++ Shape_Collage/mathhelper.h
@@ -13,6 +13,13 @@ class MathHelper
 {
 public:
     MathHelper();
+    /**
+     * @brief Calcul un polygone entre depart, fin avec nbsommets comme nombre de sommet
+     * @param depart Qpoint (0,0) du caré englobant le polygone
+     * @param fin Qpoint (width,height) du caré englobant le polygone
+     * @param nbsommets int representant le nombre de sommets voulu dans le polygone
+     * @return Un vecteur de points contenant tous les sommets du polygone
+     */
     static QVector<QPoint> computePolygon(QPoint, QPoint, int);
 };
 
