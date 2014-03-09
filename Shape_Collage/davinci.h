4e0e6405810436bc73a89da6334f1f89a22ff97f
Philipp Schmitt <philipp@schmitt.co>
2014-03-09 11:17:49 +0100

    Add comments

--- Shape_Collage/davinci.h
+++ Shape_Collage/davinci.h
@@ -21,13 +21,34 @@ public:
     DaVinci(QObject *parent = 0);
     DaVinci(QLabel*&, QObject *parent = 0);
     DaVinci(const DaVinci&);
+    /**
+     * @brief Fontion de dessin
+     * @return true si le collage a été généré sans erreur
+     */
     bool draw(Parameters, QProgressBar*&);
+    /**
+     * @brief Fonction d'export d'image
+     * @return true si l'enregistrement s'est bien passé
+     */
     bool exportImage(QString);
+    /**
+     * @brief Fonction permettant de vérifier si un collage a déjà été généré
+     * @return true si le collage a déjà été généré
+     */
     bool getAlreadyGenerated() const;
 
 private:
+    /**
+     * @brief Label sur lequel on dessine
+     */
     QLabel *mCanvas;
+    /**
+     * @brief Si le collage a été généré
+     */
     bool mAlreadyGenerated;
+    /**
+     * @brief Pixmap final, qui sera affiché dans le label et/ou enregistré lors de l'export
+     */
     QPixmap* mFinalPixmap;
 signals:
 
